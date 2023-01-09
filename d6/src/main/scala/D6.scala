2cabef8667d9b7bafff0f61f12cb69bf631a2618
VALux <olex3000sp@gmail.com>
2023-01-09 16:04:21 +0200

    d6 small refactoring

--- d6/src/main/scala/D6.scala
+++ d6/src/main/scala/D6.scala
@@ -2,14 +2,16 @@ import scala.annotation.tailrec
 
 @main def entrypoint() =
   val input = FileLoader.readFile("input.txt")
-  val result = Scanner.locateDuplicate(input.head.toCharArray.toList)
-  println(result)
+  val resultPart1 = Scanner.locateDuplicate(input.head.toCharArray.toList, 4)
+  val resultPart2 = Scanner.locateDuplicate(input.head.toCharArray.toList, 14)
+  println(s"Part 1: $resultPart1")
+  println(s"Part 2: $resultPart2")
 
 object Scanner:
-  def locateDuplicate(input: List[Char]): Int =
+  def locateDuplicate(input: List[Char], windowSize: Int): Int =
     def containsDuplicates(chunk: List[Char]): Boolean =
       chunk.toSet.size == chunk.size
 
-    input.zipWithIndex.sliding(14).find(chunk => containsDuplicates(chunk.map(_._1))) match
+    input.zipWithIndex.sliding(windowSize).find(chunk => containsDuplicates(chunk.map(_._1))) match
       case Some(chunk) => chunk.last._2 + 1
       case _           => -1
