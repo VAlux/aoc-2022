794f05af7a994c639689caa89f04ee7abaedecfc
VALux <olex3000sp@gmail.com>
2023-01-09 14:45:37 +0200

    day 4 part 1 done

--- d4/src/main/scala/D4.scala
+++ d4/src/main/scala/D4.scala
@@ -1,2 +1,36 @@
 @main def entrypoint() =
-  println("Hello world!")
+  import RangeExplorer.*
+  val input = FileLoader.readFile("input.txt")
+  val ranges = input.map(RangeParser.parseRangePair).flatten
+  val inclusionsAmount = ranges.count(RangeExplorer.isInclusivePair)
+  println(inclusionsAmount)
+
+case class Range(from: Int, to: Int)
+case class RangePair(left: Range, right: Range)
+
+object RangeParser:
+  def parseRange(input: String): Option[Range] =
+    input.split('-') match
+      case Array(a, b) =>
+        for
+          left <- a.toIntOption
+          right <- b.toIntOption
+        yield Range(left, right)
+      case _ => None
+
+  def parseRangePair(input: String): Option[RangePair] =
+    input.split(',') match
+      case Array(a, b) =>
+        for
+          left <- parseRange(a)
+          right <- parseRange(b)
+        yield RangePair(left, right)
+      case _ => None
+
+object RangeExplorer:
+  def isInclusivePair(pair: RangePair): Boolean =
+    (pair.right contains pair.left) || (pair.left contains pair.right)
+
+  extension (left: Range)
+    def contains(right: Range): Boolean =
+      left.from <= right.from && left.to >= right.to
