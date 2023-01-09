78fee53df9e1d6a77e80b7017e5fec8d96414c53
VALux <olex3000sp@gmail.com>
2023-01-09 14:45:37 +0200

    day 3 done

--- d3/src/main/scala/D3.scala
+++ d3/src/main/scala/D3.scala
@@ -1,8 +1,14 @@
+import scala.util.chaining.*
+
 @main def entrypoint() =
   val input = FileLoader.readFile("input.txt")
   val rucksacks = RucksackParser.parse(input)
-  val commonItemsPrioritiesSum = ItemPriority.sumPriorities(rucksacks.flatMap(findCommonItem))
+  val commonItemsPrioritiesSum = ItemPriority.sumPriorities(rucksacks.flatMap(RucksackScanner.findCommonItem))
+  val commonItemInGroupsOf3Elves =
+    rucksacks.grouped(3).flatMap(RucksackScanner.findCommonItems).toList.pipe(ItemPriority.sumPriorities)
+
   println(commonItemsPrioritiesSum)
+  println(commonItemInGroupsOf3Elves)
 
 case class Rucksack(val items: List[Char]):
   val firstCompartment = items.slice(0, items.length / 2).toSet
@@ -12,8 +18,15 @@ object RucksackParser:
   def parse(input: String): Rucksack = Rucksack(input.toCharArray.toList)
   def parse(input: List[String]): List[Rucksack] = input.map(parse)
 
-def findCommonItem(rucksack: Rucksack): Option[Char] =
-  rucksack.firstCompartment.intersect(rucksack.secondCompartment).headOption
+object RucksackScanner:
+  private def findCommonItem(first: Set[Char], second: Set[Char]): Option[Char] =
+    first.intersect(second).headOption
+
+  def findCommonItem(rucksack: Rucksack): Option[Char] =
+    findCommonItem(rucksack.firstCompartment, rucksack.secondCompartment)
+
+  def findCommonItems(rucksacks: List[Rucksack]): Set[Char] =
+    rucksacks.map(_.items.toSet).reduce(_ intersect _)
 
 object ItemPriority:
   private val priorities: Map[Char, Int] =
