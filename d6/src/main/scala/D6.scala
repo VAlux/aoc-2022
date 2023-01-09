3ed1edb327607e7e5807f2b941fe4f5e0b6ab937
VALux <olex3000sp@gmail.com>
2023-01-09 14:45:37 +0200

    day 6 part 1 done

--- d6/src/main/scala/D6.scala
+++ d6/src/main/scala/D6.scala
@@ -2,3 +2,14 @@ import scala.annotation.tailrec
 
 @main def entrypoint() =
   val input = FileLoader.readFile("input.txt")
+  val result = Scanner.locateDuplicate(input.head.toCharArray.toList)
+  println(result)
+
+object Scanner:
+  def locateDuplicate(input: List[Char]): Int =
+    def containsDuplicates(chunk: List[Char]): Boolean =
+      chunk.toSet.size == chunk.size
+
+    input.zipWithIndex.sliding(14).find(chunk => containsDuplicates(chunk.map(_._1))) match
+      case Some(chunk) => chunk.last._2 + 1
+      case _           => -1
